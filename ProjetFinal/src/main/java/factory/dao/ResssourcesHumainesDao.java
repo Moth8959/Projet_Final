e67b7e2ea8a51bbcca80d07436869104021d5687
Timothée <timothee.moliere@gmail.com>
2018-06-18 17:32:59 +0200

    suppression de l'interface RessourcesHumainesDa

--- ProjetFinal/src/main/java/factory/dao/ResssourcesHumainesDao.java
+++
@@ -1,9 +0,0 @@
-package factory.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import factory.model.RessourcesHumaines;
-
-public interface ResssourcesHumainesDao extends JpaRepository< RessourcesHumaines, Long> {
-
-}
