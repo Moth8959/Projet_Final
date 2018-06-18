67162de0f9df4bb6465f619e7f835f5510c07bac
Timothée <timothee.moliere@gmail.com>
2018-06-18 11:25:17 +0200

    ajout projet Spring Boot

---
+++ ProjetFinal/src/main/java/factory/FormationBootApplication.java
@@ -0,0 +1,12 @@
+package factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationBootApplication.class, args);
+	}
+}
