bd25116be51c102c06a99cfa5fa399a70a5b8bcd
Julien <julien-musman@orange.fr>
2018-06-18 15:50:36 +0200

    constructeurs formateur

--- ProjetFinal/src/main/java/factory/model/Formateur.java
+++ ProjetFinal/src/main/java/factory/model/Formateur.java
@@ -3,23 +3,16 @@ package factory.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Version;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import com.fasterxml.jackson.annotation.JsonView;
 
 @Entity
@@ -42,6 +35,62 @@ public class Formateur extends RessourcesHumaines {
 
 	public Formateur() {
 		super();
+	}
+
+	
+	
+	public Formateur(String nom, String prenom, String coordonnées, String rue, String ville, Integer codePostal,
+			Boolean accesAdministrateur, Boolean accesFormateur, Boolean accesStagiaire, Boolean accesTechnicien,
+			String username, String password) {
+		super(nom, prenom, coordonnées, rue, ville, codePostal, accesAdministrateur, accesFormateur, accesStagiaire,
+				accesTechnicien, username, password);
+	}
+
+	
+
+	public Formateur(String nom, String prenom, String coordonnées, String rue, String ville, Integer codePostal,
+			Boolean accesAdministrateur, Boolean accesFormateur, Boolean accesStagiaire, Boolean accesTechnicien,
+			String username, String password, Long id, List<Matiere> matieres, Formation formation, Module module) {
+		this(nom, prenom, coordonnées, rue, ville, codePostal, accesAdministrateur, accesFormateur, accesStagiaire,
+				accesTechnicien, username, password);
+		this.id = id;
+		this.matieres = matieres;
+		this.formation = formation;
+		this.module = module;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<Matiere> getMatieres() {
+		return matieres;
+	}
+
+	public void setMatieres(List<Matiere> matieres) {
+		this.matieres = matieres;
+	}
+
+	public Formation getFormation() {
+		return formation;
+	}
+
+	public void setFormation(Formation formation) {
+		this.formation = formation;
+	}
+
+	public Module getModule() {
+		return module;
+	}
+
+	public void setModule(Module module) {
+		this.module = module;
 	}
 
 	
