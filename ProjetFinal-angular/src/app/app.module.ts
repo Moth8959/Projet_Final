eb6dc4b08784fc650fe85d5a78ed7860baef21cd
Unknown <timothee.moliere@gmail.com>
2018-06-19 01:31:30 +0200

    MaJ site web

--- ProjetFinal-angular/src/app/app.module.ts
+++ ProjetFinal-angular/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './Divers/contact.component';
+import { HomeComponent } from './home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
