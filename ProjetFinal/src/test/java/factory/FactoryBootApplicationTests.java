ba1116f890f8568303d93fba14ccc078edf86745
Timothée <timothee.moliere@gmail.com>
2018-06-19 14:51:26 +0200

    MaJ FactoryBootApplicationTests

--- ProjetFinal/src/test/java/factory/FactoryBootApplicationTests.java
+++ ProjetFinal/src/test/java/factory/FactoryBootApplicationTests.java
@@ -8,10 +8,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-
-import factory.dao.*;
-import factory.model.*;
+import factory.dao.FormateurDao;
+import factory.dao.FormationDao;
+import factory.dao.GestionnaireDao;
+import factory.dao.MatiereDao;
+import factory.dao.ModuleDao;
+import factory.dao.OrdinateurDao;
+import factory.dao.SalleDao;
+import factory.dao.StagiaireDao;
+import factory.dao.TechnicienDao;
+import factory.dao.VideoProjecteurDao;
+import factory.model.Formateur;
+import factory.model.Matiere;
+import factory.model.Module;
+import factory.model.NivEnum;
+import factory.model.Ordinateur;
+import factory.model.Salle;
+import factory.model.Technicien;
+import factory.model.VideoProjecteur;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -50,13 +64,30 @@ public class FactoryBootApplicationTests {
 		salleDao.save(salle2);
 		salleDao.save(salle3);
 		
-		VideoProjecteur videoProjecteur = new VideoProjecteur("Acer", 20d, "1920x1080", true, true);
-		videoProjecteurDao.save(videoProjecteur);
+		VideoProjecteur videoProj1 = new VideoProjecteur("Acer", 20d, "1920x1080", true, true);
+		videoProjecteurDao.save(videoProj1);
+		
+		Ordinateur ordi1 = new Ordinateur("AJC 160", 15d, "i5", 16, 500, 2016);
+		Ordinateur ordi2 = new Ordinateur("AJC 168", 15d, "i5", 16, 500, 2016);
+		ordinateurDao.save(ordi1);
+		ordinateurDao.save(ordi2);
+		
+		Matiere mat1 = new Matiere("Spring Boot", NivEnum.Débutant, "connaître Spring Boot", "blabla de contenu");
+		Matiere mat2 = new Matiere("Boostrap", NivEnum.Avancé, "être autonome sur Boostrap", "blabla de contenu");
+		matiereDao.save(mat1);
+		matiereDao.save(mat2);
+		
+		Formateur eric = new Formateur();
+		formateurDao.save(eric);
+		
+		
+		
  		
 		
 		
-		Module module = new Module(salle1, new Date(), new Date());
+		Module module = new Module(mat1, salle1, eric, new Date(), new Date(), videoProj1);
 		Technicien technicien = new Technicien("Terieur", "Alain", "alain.terieur@ajc.fr", "6 rue Rougemont", "Paris", "75008");
+		technicienDao.save(technicien);
 		moduleDao.save(module);
 		
 		
