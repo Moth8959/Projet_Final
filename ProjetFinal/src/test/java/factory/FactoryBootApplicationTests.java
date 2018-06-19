1c4e2a19c045b7edc27bcf65512a20c5fec53520
Timothée <timothee.moliere@gmail.com>
2018-06-19 10:44:00 +0200

    Modification de FactoryBootApplicationTests

--- ProjetFinal/src/test/java/factory/FactoryBootApplicationTests.java
+++ ProjetFinal/src/test/java/factory/FactoryBootApplicationTests.java
@@ -8,11 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-
 import factory.dao.ModuleDao;
-import factory.model.Salle;
+import factory.dao.SalleDao;
+import factory.dao.VideoProjecteurDao;
 import factory.model.Module;
+import factory.model.Salle;
+import factory.model.VideoProjecteur;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,12 +21,24 @@ public class FactoryBootApplicationTests {
 	
 	@Autowired
 	private ModuleDao moduleDao;
+	@Autowired
+	private SalleDao salleDao;
+	
 
 
 	@Test
 	public void contextLoads() {
 		
-		Module module = new Module(new Salle("gcffy", 10.2d,4), new Date(), new Date());
+		Salle salle1 = new Salle("Saphir", 100d, 15);
+		Salle salle2 = new Salle("Emeraude", 80d, 20);
+		Salle salle3 = new Salle("Ebène", 50d, 10);
+		salleDao.save(salle1);
+		salleDao.save(salle2);
+		salleDao.save(salle3);
+		
+		
+		
+		Module module = new Module(salle1, new Date(), new Date());
 		moduleDao.save(module);
 		
 		
