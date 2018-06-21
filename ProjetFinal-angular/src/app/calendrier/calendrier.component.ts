51dee3eb036e29a80189d81f06d92e94fcf0499d
Unknown <timothee.moliere@gmail.com>
2018-06-21 02:30:11 +0200

    MaJ Calendrier

--- ProjetFinal-angular/src/app/calendrier/calendrier.component.ts
+++ ProjetFinal-angular/src/app/calendrier/calendrier.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import { CalendarComponent } from 'ng-fullcalendar';
 import { Options } from 'fullcalendar';
 import { EventService } from '../services/event.service';
+import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-calendrier',
@@ -12,6 +13,7 @@ export class CalendrierComponent implements OnInit {
   calendarOptions: Options;
   displayEvent: any;
   events = null;
+  isWeekend = true;
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
   constructor(protected eventService: EventService) {
 
@@ -22,20 +24,49 @@ export class CalendrierComponent implements OnInit {
 
   ngOnInit() {
     this.calendarOptions = {
+      locale: 'fr',
       editable: true,
       eventLimit: false,
+      weekends: this.isWeekend,
+      businessHours: true,
       header: {
         left: 'prev,next today',
         center: 'title',
         right: 'month,agendaWeek,agendaDay,listMonth'
       },
-      events: []
+      events: [{
+        title: 'Angular 4',
+        start: '2018-06-11',
+        end: '2018-06-15',
+        color: 'red',
+      },
+        {
+        title: 'Projet Final',
+        start: '2018-06-15',
+        end: '2018-06-22',
+        color: 'yellow',
+      },
+        {
+          start: '2018-06-25',
+          end: '2018-06-25',
+          overlap: false,
+          rendering: 'background',
+          color: '#ff9f89'
+        }]
     };
   }
   loadevents() {
     this.eventService.getEvents().subscribe(data => {
       this.calendarOptions.events = data;
     });
+    console.log('méthode loadevents() entrée');
+/*    console.log(this.eventService.getEvents().value);*/
+    this.events = {
+      title: 'Projet de groupe',
+      start: '2018-06-15',
+      end: '2018-06-21',
+      };
+
   }
   clickButton(model: any) {
     this.displayEvent = model;
@@ -71,5 +102,21 @@ export class CalendrierComponent implements OnInit {
       }
     }
     this.displayEvent = model;
+  }
+  chgWeekend(isWeekend: boolean) {
+    if (isWeekend) {
+      isWeekend = false;
+      console.log(isWeekend);
+    } else {
+      isWeekend = true;
+    }
+
+  }
+
+  pdfDownload() {
+    console.log('pdfDownload() entered');
+    html2canvas(document.body).then((canvas) => {
+      window.open().document.write('<img src="' + canvas.toDataURL() + '" />');
+    });
   }
 }
